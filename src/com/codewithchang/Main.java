569a6ce1ca6f639300de2e07bb230d7ee65e6d86
Chang <cnxiong03@gmail.com>
2021-03-11 23:43:51 -0500

    overload method created to calculate surface area of rectangle

--- src/com/codewithchang/Main.java
+++ src/com/codewithchang/Main.java
@@ -5,6 +5,9 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println(area(5));
+        System.out.println(area(-1));
+        System.out.println(area(5.0, 4.0));
+        System.out.println(area(-1.0, 4.0));
     }
 
    public static double area(double radius) {
@@ -12,5 +15,12 @@ public class Main {
             return -1;
         }
         return (radius * radius) * PI;
+   }
+
+   public static double area(double x, double y) {
+        if((x < 0) || (y < 0)) {
+            return -1;
+        }
+        return x * y;
    }
 }
